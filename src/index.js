2ae72853dfbb957a85817857e9289777acfe1150
tomas <josetomashervas@gmail.com>
2022-07-29 17:35:12 +0200

    cambio variable puerto

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const miticos = require('./miticos.json');
 const legendarios = require('./legendarios.json') 
  
-const port = process.env.PORT || 3000;
+const port = `0.0.0.0:$PORT`;
+const validoHasta = new Date(2022,7,31).getTime()
+const nuevos = [150,151]
+
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {    
     res.json(
         {
-            miticos,legendarios
+            validoHasta,
+            nuevos,
+            miticos,
+            legendarios 
         }
     );
 })
