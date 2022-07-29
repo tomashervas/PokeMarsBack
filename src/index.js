e7f5798d759c79f228c51d479f2bb4e56ac1eb04
tomas <josetomashervas@gmail.com>
2022-07-29 18:18:14 +0200

    prueba

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -6,15 +5,12 @@ const miticos = require('./miticos.json');
 const legendarios = require('./legendarios.json') 
  
 const port = process.env.PORT || 3333;
-console.log(port)
-console.log(process.env.PORT)
 const validoHasta = new Date(2022,7,31).getTime()
 const nuevos = [150,151]
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
-app.use(bodyParser.text({ type: "text/html" }));
+
 
 app.get('/',async (req, res) => {    
     res.json(
